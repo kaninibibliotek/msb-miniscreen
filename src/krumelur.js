dbe5d907c3143812414845cc8ff6ea9baccd8baa
Henrik <henrik@unswornindustries.com>
2016-05-10 10:57:03 +0200

    Krumelur animation prep

--- src/krumelur.js
+++ src/krumelur.js
@@ -1,14 +1,42 @@
 var krumelur = (function() {
-  // canvas animation or img with css keyframes?
   var canvasElem = document.getElementsByClassName('krumelur')[0];
+  var context    = canvasElem.getContext('2d');
+  var loop       = false;
+  var image;
+
+  function draw() {
+    if (loop) {
+      requestAnimationFrame(draw);
+
+      context.clearRect(0, 0, 800, 600);
+
+      context.drawImage(image, 0, 0, 800, 600);
+    }
+  }
+
+  function start() {
+    loop = true;
+    requestAnimationFrame(draw);
+  }
+
+  function stop() {
+    loop = false;
+  }
 
   return {
     show: function(source) {
       removeClass(canvasElem, 'hidden');
+
+      image     = new Image();
+      image.src = source;
+
+      start();
     },
 
     hide: function() {
       addClass(canvasElem, 'hidden');
+
+      stop();
     }
   }
 })();
