66865c6ec2ede66aadfc162667166d85f287e557
Henrik <henrik@unswornindustries.com>
2016-05-10 17:45:28 +0200

    Indentation

--- src/main.js
+++ src/main.js
@@ -6,33 +6,33 @@ function onReceivedPlaylist(playlist) {
 setTimeout(function() {
   var playlist = [
     {
-        type: 'video',
-        source: '../test/movie.mp4',
-        duration: 2
-      },
-      {
-        type: 'video',
-        source: '../test/movie2.mov',
-        duration: 2
-      },
-      {
-        type: 'image',
-        source: '../test/image.jpg',
-        duration: 3,
-        animation: 'spin'
-      },
-      {
-        type: 'image',
-        source: '../test/image2.jpg',
-        duration: 4,
-        animation: 'squeeze'
-      },
-      {
-        type: 'image',
-        source: '../test/krumelur.jpg',
-        duration: 4,
-        animation: 'jump'
-      }
+      type: 'video',
+      source: '../test/movie.mp4',
+      duration: 2
+    },
+    {
+      type: 'video',
+      source: '../test/movie2.mov',
+      duration: 2
+    },
+    {
+      type: 'image',
+      source: '../test/image.jpg',
+      duration: 3,
+      animation: 'spin'
+    },
+    {
+      type: 'image',
+      source: '../test/image2.jpg',
+      duration: 4,
+      animation: 'squeeze'
+    },
+    {
+      type: 'image',
+      source: '../test/krumelur.jpg',
+      duration: 4,
+      animation: 'jump'
+    }
   ];
 
   onReceivedPlaylist(playlist);
