3e5dfb5ef48ee53c531071be265c3c490d23ee42
Henrik <henrik@unswornindustries.com>
2016-05-16 11:17:31 +0200

    Use mock server

--- src/main.js
+++ src/main.js
@@ -2,38 +2,16 @@ function onReceivedPlaylist(playlist) {
   player.setPlaylist(playlist);
 }
 
-// Fake received playlist
-setTimeout(function() {
-  var playlist = [
-    {
-      type: 'video',
-      source: 'test/movie.mp4',
-      duration: 2
-    },
-    {
-      type: 'video',
-      source: 'test/movie2.mov',
-      duration: 2
-    },
-    {
-      type: 'image',
-      source: 'test/image.jpg',
-      duration: 3,
-      animation: 'spin'
-    },
-    {
-      type: 'image',
-      source: 'test/image2.jpg',
-      duration: 4,
-      animation: 'squeeze'
-    },
-    {
-      type: 'image',
-      source: 'test/krumelur.jpg',
-      duration: 4,
-      animation: 'jump'
-    }
-  ];
+var request = new XMLHttpRequest();
 
-  onReceivedPlaylist(playlist);
-}, 100);
+request.onreadystatechange = function() {
+  if (request.readyState === 1) {
+    request.send();
+  }
+
+  if (request.readyState === 4 && request.status == 200) {
+    onReceivedPlaylist(JSON.parse(request.response));
+  }
+};
+
+request.open('GET', '/miniscreen?id=' + config.id);
