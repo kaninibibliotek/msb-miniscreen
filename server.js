fc51661d2776dc6fe286b9f5dadccbe58bf33fce
Henrik <henrik@unswornindustries.com>
2016-05-16 11:17:23 +0200

    Mock express server

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
-var connect     = require('connect');
-var serveStatic = require('serve-static');
+var express = require('express');
+var fs      = require('fs');
 
-var app = connect();
+var media = [];
 
-app.use(serveStatic(__dirname)).listen(3000, function() {
-    console.log('Server running on 3000');
+fs.readdir(__dirname + '/mediajson', function(err, files) {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach(function(file) {
+      fs.readFile(__dirname + '/mediajson/' + file, 'utf8', function(err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          media.push(JSON.parse(data));
+        }
+      });
+    });
+  }
+});
+
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/miniscreen', function(req, res) {
+  console.log("GET miniscreen", req.query.id);
+
+  res.send(media);
+});
+
+app.listen(3000, function() {
+  console.log('Server running on 3000');
 });
