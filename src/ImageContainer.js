9781140c15d767b78adc4948402d42b661515432
Henrik <henrik@unswornindustries.com>
2016-05-24 14:27:07 +0200

    KrumelurContainer

--- src/ImageContainer.js
+++ src/ImageContainer.js
@@ -1,29 +1,16 @@
 function ImageContainer(containerElem, imageElem) {
   this.container = containerElem;
   this.image     = imageElem;
-  this.animation = null;
 }
 
-ImageContainer.prototype.load = function(source, animation) {
-  this.animation = animation;
+ImageContainer.prototype.load = function(source) {
   this.image.src = source;
 };
 
 ImageContainer.prototype.show = function() {
   utils.removeClass(this.container, 'hidden');
-
-  if (this.animation) {
-    // Trigger reflow, needed in case the same image is showed twice in a row
-    this.image.height;
-
-    utils.addClass(this.image, this.animation);
-  }
 };
 
 ImageContainer.prototype.hide = function() {
   utils.addClass(this.container, 'hidden');
-
-  if (this.animation) {
-    utils.removeClass(this.image, this.animation);
-  }
 };
